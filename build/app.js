2f09f7756853a1cb88b9a54cb73046c76b2ee152
Dexter Esajas <dunk_king7@hotmail.com>
2016-03-14 17:27:55 +0100

    Adding the empty function

--- build/app.js
+++ build/app.js
@@ -1,113 +1,117 @@
-(function() {
-  var Backbone, Default, _,
-    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+var Backbone, Default, _,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
 
-  window.$ = require('jquery');
+window.$ = require('jquery');
 
-  _ = require('lodash');
+_ = require('lodash');
 
-  Backbone = require('backbone');
+Backbone = require('backbone');
 
-  if (!window.Vent) {
-    window.Vent = _["extends"]({}, Backbone.Events);
+if (!window.Vent) {
+  window.Vent = _["extends"]({}, Backbone.Events);
+}
+
+Default = (function(superClass) {
+  extend(Default, superClass);
+
+  function Default() {
+    this.show = bind(this.show, this);
+    this.hide = bind(this.hide, this);
+    this.empty = bind(this.empty, this);
+    this.quit = bind(this.quit, this);
+    this._default = {
+      events: [],
+      children: []
+    };
+    Default.__super__.constructor.apply(this, arguments);
   }
 
-  Default = (function(superClass) {
-    extend(Default, superClass);
+  Default.prototype.on = function(eventName, func) {
+    this._default.events.push([eventName, func]);
+    return Vent.on(eventName, func);
+  };
 
-    function Default() {
-      this.show = bind(this.show, this);
-      this.hide = bind(this.hide, this);
-      this.quit = bind(this.quit, this);
-      this._default = {
-        events: [],
-        children: []
-      };
-      Default.__super__.constructor.apply(this, arguments);
+  Default.prototype.once = function(eventName, func) {
+    this._default.events.push([eventName, func]);
+    return Vent.once(eventName, func);
+  };
+
+  Default.prototype.off = function(eventName, func) {
+    var event, i, len, ref, results;
+    if (eventName) {
+      return Vent.off(eventName, func);
     }
+    ref = this._default.events;
+    results = [];
+    for (i = 0, len = ref.length; i < len; i++) {
+      event = ref[i];
+      results.push(Vent.off(event[0], event[1]));
+    }
+    return results;
+  };
 
-    Default.prototype.on = function(eventName, func) {
-      this._default.events.push([eventName, func]);
-      return Vent.on(eventName, func);
-    };
+  Default.prototype.append = function(view) {
+    this._default.children.push(view);
+    return this.$el.append(view.el);
+  };
 
-    Default.prototype.once = function(eventName, func) {
-      this._default.events.push([eventName, func]);
-      return Vent.once(eventName, func);
-    };
+  Default.prototype.preppend = function(view) {
+    this._default.children.push(view);
+    return this.$el.prepend(view.el);
+  };
 
-    Default.prototype.off = function(eventName, func) {
-      var event, i, len, ref, results;
-      if (eventName) {
-        return Vent.off(eventName, func);
-      }
-      ref = this._default.events;
-      results = [];
-      for (i = 0, len = ref.length; i < len; i++) {
-        event = ref[i];
-        results.push(Vent.off(event[0], event[1]));
-      }
-      return results;
-    };
+  Default.prototype.block = function(e) {
+    e.preventDefault();
+    return e.stopPropagation();
+  };
 
-    Default.prototype.append = function(view) {
-      this._default.children.push(view);
-      return this.$el.append(view.el);
-    };
+  Default.prototype.quit = function() {
+    this.empty();
+    this.off();
+    return this.remove();
+  };
 
-    Default.prototype.preppend = function(view) {
-      this._default.children.push(view);
-      return this.$el.prepend(view.el);
-    };
+  Default.prototype.empty = function() {
+    var child, i, len, ref, results;
+    ref = this._default.children;
+    results = [];
+    for (i = 0, len = ref.length; i < len; i++) {
+      child = ref[i];
+      results.push(child.quit());
+    }
+    return results;
+  };
 
-    Default.prototype.block = function(e) {
-      e.preventDefault();
-      return e.stopPropagation();
-    };
+  Default.prototype.hide = function() {
+    return this.$el.removeClass('show-me');
+  };
 
-    Default.prototype.quit = function() {
-      var child, i, len, ref;
-      ref = this._default.children;
-      for (i = 0, len = ref.length; i < len; i++) {
-        child = ref[i];
-        child.quit();
-      }
-      this.off();
-      return this.remove();
-    };
+  Default.prototype.show = function() {
+    return this.$el.addClass('show-me');
+  };
 
-    Default.prototype.hide = function() {
-      return this.$el.removeClass('show-me');
-    };
+  Default.prototype.trigger = function() {
+    return Vent.trigger.apply(Vent, arguments);
+  };
 
-    Default.prototype.show = function() {
-      return this.$el.addClass('show-me');
-    };
+  Default.prototype.leftClick = function(e) {
+    return (e != null ? e.button : void 0) === 0;
+  };
 
-    Default.prototype.trigger = function() {
-      return Vent.trigger.apply(Vent, arguments);
-    };
+  Default.prototype.objectLength = function(obj) {
+    return Object.keys(obj).length;
+  };
 
-    Default.prototype.leftClick = function(e) {
-      return (e != null ? e.button : void 0) === 0;
-    };
+  Default.prototype.enterPressed = function(e) {
+    if (e) {
+      return (e.which || e.key) === 13;
+    }
+  };
 
-    Default.prototype.objectLength = function(obj) {
-      return Object.keys(obj).length;
-    };
+  return Default;
 
-    Default.prototype.enterPressed = function(e) {
-      if (e) {
-        return (e.which || e.key) === 13;
-      }
-    };
+})(Backbone.View);
 
-    return Default;
-
-  })(Backbone.View);
-
-  module.exports = Default;
-
-}).call(this);
+module.exports = Default;
